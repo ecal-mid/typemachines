7fd8e2e7b545442fe0a1b20c9f7b0bee9a275689
PTRRR <pietro.alberti@bluewin.ch>
2020-05-12 16:22:16 +0200

    Improve grid example

--- grid3d/grid3d.pde
+++ grid3d/grid3d.pde
@@ -4,8 +4,8 @@ PImage img;
 
 void setup() {
   size(1000, 1000, P3D);
-  numX = 10;
-  numY = 10;
+  numX = 20;
+  numY = 20;
   img = loadImage("img.jpeg");
   rectMode(CENTER);
   imageMode(CENTER);
@@ -14,25 +14,29 @@ void setup() {
 void draw() {
   background(0);
 
+  int stepX = width / numX;
+  int stepY = height / numY;
+
+  int imageWidth = img.width / numX;
+  int imageHeight = img.height / numY;
+
   for (int j = 0; j < numY; j++) {
     for (int i = 0; i < numX; i++) {
-      int stepX = width / numX;
-      int stepY = height / numY;
       int x = stepX * i;
       int y = stepY * j;
 
-      push();
-      float varyingValue = sin(millis() * 0.001 + i * 0.2 + j * 0.2);
-      translate(x + stepX * 0.5, y + stepY * 0.5, varyingValue * 100);
-      rotateY(varyingValue * 0.5);
-
-      int imageWidth = img.width / numX;
-      int imageHeight = img.height / numY;
       int imageX = imageWidth * i;
       int imageY = imageHeight * j;
 
+      push();
+      float varyingValue = sin(millis() * 0.001 + i * 0.2 + j * 0.2);
+
+      translate(x + stepX * 0.5, y + stepY * 0.5, varyingValue * 100);
+      rotateY(varyingValue);
+
       PImage partOfImage = img.get(imageX, imageY, imageWidth, imageHeight);
       image(partOfImage, 0, 0, stepX, stepY);
+
       pop();
     }
   }
--- grid3d/out/source/grid3d.java
+++ grid3d/out/source/grid3d.java
@@ -20,8 +20,8 @@ PImage img;
 
 public void setup() {
   
-  numX = 10;
-  numY = 10;
+  numX = 20;
+  numY = 20;
   img = loadImage("img.jpeg");
   rectMode(CENTER);
   imageMode(CENTER);
@@ -30,25 +30,29 @@ public void setup() {
 public void draw() {
   background(0);
 
+  int stepX = width / numX;
+  int stepY = height / numY;
+
+  int imageWidth = img.width / numX;
+  int imageHeight = img.height / numY;
+
   for (int j = 0; j < numY; j++) {
     for (int i = 0; i < numX; i++) {
-      int stepX = width / numX;
-      int stepY = height / numY;
       int x = stepX * i;
       int y = stepY * j;
 
-      push();
-      float varyingValue = sin(millis() * 0.001f + i * 0.2f + j * 0.2f);
-      translate(x + stepX * 0.5f, y + stepY * 0.5f, varyingValue * 100);
-      rotateY(varyingValue * 0.5f);
-
-      int imageWidth = img.width / numX;
-      int imageHeight = img.height / numY;
       int imageX = imageWidth * i;
       int imageY = imageHeight * j;
 
+      push();
+      float varyingValue = sin(millis() * 0.001f + i * 0.2f + j * 0.2f);
+
+      translate(x + stepX * 0.5f, y + stepY * 0.5f, varyingValue * 100);
+      rotateY(varyingValue);
+
       PImage partOfImage = img.get(imageX, imageY, imageWidth, imageHeight);
       image(partOfImage, 0, 0, stepX, stepY);
+
       pop();
     }
   }
